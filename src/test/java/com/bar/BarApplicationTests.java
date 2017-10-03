f5a9c9f3dc9d41789e4618c6f45e6d915098ba16
vladimir.sulevskiy <vsu@conciliolabs.com>
2017-10-03 18:53:35 +0300

    Heroku

--- src/test/java/com/bar/BarApplicationTests.java
+++ src/test/java/com/bar/BarApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.bar;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class BarApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
-
+    @Test
+    public void contextLoads() {
+    }
 }
