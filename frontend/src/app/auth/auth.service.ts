e7b32f2ccb45b8ee251025a492d8418ee351e4e6
Sergey Troinin <sergey.troynin@agileengine.com>
2017-11-08 10:40:20 +0200

    Declare types for Auth0

--- frontend/src/app/auth/auth.service.ts
+++ frontend/src/app/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { Router } from "@angular/router";
 import "rxjs/add/operator/filter";
 import * as auth0 from "auth0-js";
 import { environment } from '../../environments/environment';
+import { Auth0DecodedHash, Auth0Error } from "auth0-js";
 
 @Injectable()
 export class AuthService {
@@ -23,7 +24,7 @@ export class AuthService {
   }
 
   public handleAuthentication(): void {
-    this.auth0.parseHash((err, authResult) => {
+    this.auth0.parseHash((err: Auth0Error, authResult: Auth0DecodedHash) => {
       if (authResult && authResult.accessToken && authResult.idToken) {
         window.location.hash = "";
         this.setSession(authResult);
@@ -35,7 +36,7 @@ export class AuthService {
     });
   }
 
-  private setSession(authResult): void {
+  private setSession(authResult: Auth0DecodedHash): void {
     const expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
     localStorage.setItem("access_token", authResult.accessToken);
     localStorage.setItem("id_token", authResult.idToken);
